1548ead224c269031eb80d2ba81ffe2c7916bd8d
Adrian Karwowski <adziok1998@gmail.com>
2023-12-03 09:32:45 +0100

    D3 init

---
+++ d3/lib/d3.ex
@@ -0,0 +1,18 @@
+defmodule D3 do
+  @moduledoc """
+  Documentation for `D3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
