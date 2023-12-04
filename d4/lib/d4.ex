f84cff5682105d6c60ac04e1489f7cd4146f835c
Adrian Karwowski <adziok1998@gmail.com>
2023-12-04 08:28:07 +0100

    D4 init

---
+++ d4/lib/d4.ex
@@ -0,0 +1,18 @@
+defmodule D4 do
+  @moduledoc """
+  Documentation for `D4`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D4.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
