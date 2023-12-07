6d2bae4cfae52fe8cebefa9adc8ce546b1d05a58
Adrian Karwowski <adziok1998@gmail.com>
2023-12-07 17:27:53 +0100

    D7 INIT

---
+++ d7/lib/d7.ex
@@ -0,0 +1,18 @@
+defmodule D7 do
+  @moduledoc """
+  Documentation for `D7`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D7.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
